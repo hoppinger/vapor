e7e48bf8cf55448bd35e3635427a730c39ba97c0
Dunya Kirkali <dunyakirkali@gmail.com>
2015-09-25 12:31:20 +0200

    Configure rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require "bundler/setup"
+Bundler.setup
+
+require "vapor"
+
+RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+  config.order = "random"
+end
