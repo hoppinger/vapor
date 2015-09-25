12dce4e7736a64f71243a4c7144f47d5305f4002
Dunya Kirkali <dunyakirkali@gmail.com>
2015-09-25 17:11:09 +0200

    Improve configuration spec

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -2,20 +2,29 @@ require "spec_helper"
 
 RSpec.describe Vapor::Configuration do
   let(:vapor) { Vapor }
+  let(:configuration) { vapor.configuration }
+
+  before do
+    Vapor.configure do |config|
+      config.base_url = 'http://www.hoppinger.com'
+      config.username = 'username'
+      config.password = 'password'
+    end
+  end
 
   it "should have a default configuration" do
-    expect(vapor.configuration).not_to eq(nil)
+    expect(configuration).not_to eq(nil)
   end
 
   it "should have a default base_url" do
-    expect(vapor.configuration.base_url).not_to eq("http://www.hoppinger.com")
+    expect(configuration.base_url).to eq("http://www.hoppinger.com")
   end
 
   it "should have a default username" do
-    expect(vapor.configuration.username).not_to eq("username")
+    expect(configuration.username).to eq("username")
   end
 
   it "should have a default password" do
-    expect(vapor.configuration.password).not_to eq("password")
+    expect(configuration.password).to eq("password")
   end
 end
